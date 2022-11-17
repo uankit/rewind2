3a46111922bb9c562f705d2bff9a3ab8109c6d2a
Ankit Upadhyay <uankitu@gmail.com>
2022-11-17 19:38:15 +0000

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -2,16 +2,13 @@ function onSubmit() {
     let caseStory = document.getElementById("case").value;
     console.log(caseStory);
     if (caseStory == "101") {
-        alert("Best of luck")
-        window.location.replace("case1.html")
+        alert("Go to /case1.html")
         return false
     } else if (caseStory == "202") {
-        alert("Best of luck")
-        window.location.replace("case2.html")
+        alert("Go to /case2.html")
         return false;
     } else if (caseStory == "303") {
-        alert("Best of luck")
-        window.location.replace("case3.html")
+        alert("Go to /case3.html")
         return false;
     } else {
         alert("You are not authorized to access this case. Please try another case number")
