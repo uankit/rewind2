51350d3314b04bf671fb17bd561c9d22aded1297
Ankit Upadhyay <uankitu@gmail.com>
2022-11-17 19:06:42 +0000

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -3,13 +3,13 @@ function onSubmit() {
     console.log(caseStory);
     if (caseStory == "101") {
         alert("Best of luck")
-        window.location = "case1.html"
-        return false;
+        window.location.href = "https://flourishing-macaron-2aa3ac.netlify.app/case1.html"
+        return false
     } else if (caseStory == "202") {
-        window.location = "case2.html"
+        window.location.href = "https://flourishing-macaron-2aa3ac.netlify.app/case2.html"
         return false;
     } else if (caseStory == "303") {
-        window.location = "case3.html"
+        window.location.href = "https://flourishing-macaron-2aa3ac.netlify.app/case3.html"
         return false;
     } else {
         alert("You are not authorized to access this case. Please try another case number")
